c9974eb64234bde6107cf2461eb8b604ef1274ba
BrendanoElTaco <33934145+BrendanoElTaco@users.noreply.github.com>
2023-11-10 12:42:03 -0500

    Added dark and light mode. Removed setters and excess getters

--- src/CalculatorUI.java
+++ src/CalculatorUI.java
@@ -1,15 +1,6 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
-import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JTextField;
+import java.awt.*;
+import javax.swing.*;
+
 
 public class CalculatorUI extends JFrame {
 	/**
@@ -45,216 +36,8 @@ public class CalculatorUI extends JFrame {
     private final Font BUTTON_FONT = new Font("SansSerif", Font.BOLD, 20);
     private final Font PI_BUTTON_FONT = new Font("SansSerif", Font.BOLD, 30);
     
-    //Getters and Setters
-    public JTextField getResultField() {
-		return resultField;
-	}
-
-	public void setResultField(JTextField resultField) {
-		this.resultField = resultField;
-	}
-
-	public JButton[] getNumButtons() {
-		return numButtons;
-	}
-
-	public void setNumButtons(JButton[] numButtons) {
-		this.numButtons = numButtons;
-	}
-
-	public JButton[] getOperationButtons() {
-		return operationButtons;
-	}
-
-	public void setOperationButtons(JButton[] operationButtons) {
-		this.operationButtons = operationButtons;
-	}
-
-	public JButton getCalculateButton() {
-		return calculateButton;
-	}
-
-	public void setCalculateButton(JButton calculateButton) {
-		this.calculateButton = calculateButton;
-	}
-
-	public JButton getSignToggleButton() {
-		return signToggleButton;
-	}
-
-	public void setSignToggleButton(JButton signToggleButton) {
-		this.signToggleButton = signToggleButton;
-	}
-
-	public JButton getSinButton() {
-		return sinButton;
-	}
-
-	public void setSinButton(JButton sinButton) {
-		this.sinButton = sinButton;
-	}
-
-	public JButton getCosButton() {
-		return cosButton;
-	}
-
-	public void setCosButton(JButton cosButton) {
-		this.cosButton = cosButton;
-	}
-
-	public JButton getTanButton() {
-		return tanButton;
-	}
-
-	public void setTanButton(JButton tanButton) {
-		this.tanButton = tanButton;
-	}
-
-	public JButton getDecimalButton() {
-		return decimalButton;
-	}
-
-	public void setDecimalButton(JButton decimalButton) {
-		this.decimalButton = decimalButton;
-	}
-
-	public JButton getPowerOfButton() {
-		return powerOfButton;
-	}
-
-	public void setPowerOfButton(JButton powerOfButton) {
-		this.powerOfButton = powerOfButton;
-	}
-
-	public JButton getBackspaceButton() {
-		return backspaceButton;
-	}
-
-	public void setBackspaceButton(JButton backspaceButton) {
-		this.backspaceButton = backspaceButton;
-	}
-
-	public JButton getCeButton() {
-		return ceButton;
-	}
-
-	public void setCeButton(JButton ceButton) {
-		this.ceButton = ceButton;
-	}
-
-	public JButton getEulerButton() {
-		return eulerButton;
-	}
-
-	public void setEulerButton(JButton eulerButton) {
-		this.eulerButton = eulerButton;
-	}
-
-	public JButton getPIButton() {
-		return PIButton;
-	}
-
-	public void setPIButton(JButton pIButton) {
-		PIButton = pIButton;
-	}
-
-	public JButton getPowerOfTenButton() {
-		return powerOfTenButton;
-	}
-
-	public void setPowerOfTenButton(JButton powerOfTenButton) {
-		this.powerOfTenButton = powerOfTenButton;
-	}
-
-	public JButton getSqrRootButton() {
-		return sqrRootButton;
-	}
-
-	public void setSqrRootButton(JButton sqrRootButton) {
-		this.sqrRootButton = sqrRootButton;
-	}
-
-	public JButton getLogBaseTenButton() {
-		return logBaseTenButton;
-	}
-
-	public void setLogBaseTenButton(JButton logBaseTenButton) {
-		this.logBaseTenButton = logBaseTenButton;
-	}
-
-	public JButton getLogBaseEButton() {
-		return logBaseEButton;
-	}
-
-	public void setLogBaseEButton(JButton logBaseEButton) {
-		this.logBaseEButton = logBaseEButton;
-	}
-
-	public JButton getSecondaryFunctionButton() {
-		return secondaryFunctionButton;
-	}
-
-	public void setSecondaryFunctionButton(JButton secondaryFunctionButton) {
-		this.secondaryFunctionButton = secondaryFunctionButton;
-	}
-
-	public JButton getxPowerOf2Button() {
-		return xPowerOf2Button;
-	}
-
-	public void setxPowerOf2Button(JButton xPowerOf2Button) {
-		this.xPowerOf2Button = xPowerOf2Button;
-	}
-
-	public JButton getSecButton() {
-		return secButton;
-	}
-
-	public void setSecButton(JButton secButton) {
-		this.secButton = secButton;
-	}
-
-	public JButton getCscButton() {
-		return cscButton;
-	}
-
-	public void setCscButton(JButton cscButton) {
-		this.cscButton = cscButton;
-	}
-
-	public JButton getCotButton() {
-		return cotButton;
-	}
-
-	public void setCotButton(JButton cotButton) {
-		this.cotButton = cotButton;
-	}
-
-	public JButton getModuloButton() {
-		return moduloButton;
-	}
-
-	public void setModuloButton(JButton moduloButton) {
-		this.moduloButton = moduloButton;
-	}
-
-	public Color getEqualBgColor() {
-		return equalBgColor;
-	}
-
-	public void setEqualBgColor(Color equalBgColor) {
-		this.equalBgColor = equalBgColor;
-	}
-
-	public Calculator getLogic() {
-		return calculator;
-	}
-
-	public void setLogic(Calculator logic) {
-		this.calculator = logic;
-	}
-
 	// Convert HTML color string to a Color object
+    // I stole these colors from the windows calculator lol
     Color bgColor = Color.decode("#f3f3f3");
     Color equalBgColor = Color.decode("#695b2e");
     Color buttonBgColor = Color.decode("#f9f9f9");
@@ -262,6 +45,7 @@ public class CalculatorUI extends JFrame {
     Color darkEqualBgColor = Color.decode("#cac585");
     Color darkNumberBgColor = Color.decode("#3b3b3b");
     Color darkButtonBgColor = Color.decode("#323232");
+    Color darkTextColor = Color.decode("#e1e1e1");
         
 	private Calculator calculator;
 
@@ -301,6 +85,10 @@ public class CalculatorUI extends JFrame {
         // Create the "Customization" menu
         JMenu customizationMenu = new JMenu("Customization");
         menuBar.add(customizationMenu);
+        
+        //Create the help menu
+        JMenu helpMenu = new JMenu("Help");
+        menuBar.add(helpMenu);
 
         // Create radio button menu items
         JRadioButtonMenuItem lightMode = new JRadioButtonMenuItem("Light Mode");
@@ -317,11 +105,10 @@ public class CalculatorUI extends JFrame {
 
         // Action listeners
         lightMode.addActionListener(e -> {
-            // Action for Option 1
+        	setLightMode();
         });
         darkMode.addActionListener(e -> {
-            mainPanel.setBackground(darkBgColor);
-            resultField.setBackground(darkBgColor);
+            setDarkMode();
             
         });
 
@@ -334,8 +121,8 @@ public class CalculatorUI extends JFrame {
         resultField = new JTextField(10);
         resultField.setEditable(false);
         resultField.setFont(new Font("SansSerif", Font.BOLD, 40));
-        resultField.setBackground(bgColor);
-        
+        resultField.setBackground(bgColor);   
+        resultField.setHorizontalAlignment(JTextField.RIGHT);
     }
 
     private void initializeNumberButtons() {
@@ -437,4 +224,217 @@ public class CalculatorUI extends JFrame {
 
         return buttonPanel;
     }
+    
+    private void setDarkMode() {
+    	mainPanel.setBackground(darkBgColor);
+        resultField.setBackground(darkBgColor);
+        for (JButton button : numButtons) {
+            button.setBackground(darkNumberBgColor);
+            button.setForeground(darkTextColor);
+        }
+        for (JButton button : operationButtons) {
+            button.setBackground(darkButtonBgColor);
+            button.setForeground(darkTextColor);
+        }
+        sinButton.setBackground(darkButtonBgColor);
+        cosButton.setBackground(darkButtonBgColor);
+        tanButton.setBackground(darkButtonBgColor);
+        decimalButton.setBackground(darkNumberBgColor);
+        signToggleButton.setBackground(darkNumberBgColor);
+        powerOfButton.setBackground(darkButtonBgColor);
+        backspaceButton.setBackground(darkButtonBgColor);
+        ceButton.setBackground(darkButtonBgColor);
+        eulerButton.setBackground(darkButtonBgColor);
+        PIButton.setBackground(darkButtonBgColor);
+        powerOfTenButton.setBackground(darkButtonBgColor);
+        sqrRootButton.setBackground(darkButtonBgColor);
+        logBaseTenButton.setBackground(darkButtonBgColor);
+        logBaseEButton.setBackground(darkButtonBgColor);
+        secondaryFunctionButton.setBackground(darkButtonBgColor);
+        xPowerOf2Button.setBackground(darkButtonBgColor);
+        secButton.setBackground(darkButtonBgColor);
+        cscButton.setBackground(darkButtonBgColor);
+        cotButton.setBackground(darkButtonBgColor);
+        moduloButton.setBackground(darkButtonBgColor);
+        calculateButton.setBackground(darkEqualBgColor);
+        
+        //Change text color to white
+        sinButton.setForeground(darkTextColor);
+        cosButton.setForeground(darkTextColor);
+        tanButton.setForeground(darkTextColor);
+        decimalButton.setForeground(darkTextColor);
+        signToggleButton.setForeground(darkTextColor);
+        powerOfButton.setForeground(darkTextColor);
+        backspaceButton.setForeground(darkTextColor);
+        ceButton.setForeground(darkTextColor);
+        eulerButton.setForeground(darkTextColor);
+        PIButton.setForeground(darkTextColor);
+        powerOfTenButton.setForeground(darkTextColor);
+        sqrRootButton.setForeground(darkTextColor);
+        logBaseTenButton.setForeground(darkTextColor);
+        logBaseEButton.setForeground(darkTextColor);
+        secondaryFunctionButton.setForeground(darkTextColor);
+        xPowerOf2Button.setForeground(darkTextColor);
+        secButton.setForeground(darkTextColor);
+        cscButton.setForeground(darkTextColor);
+        cotButton.setForeground(darkTextColor);
+        moduloButton.setForeground(darkTextColor);
+        resultField.setForeground(darkTextColor);
+        calculateButton.setForeground(darkTextColor);
+    }
+    
+    private void setLightMode() {
+    	mainPanel.setBackground(bgColor);
+        resultField.setBackground(bgColor);
+        for (JButton button : numButtons) {
+            button.setBackground(buttonBgColor);
+            button.setForeground(null);
+        }
+        for (JButton button : operationButtons) {
+            button.setBackground(buttonBgColor);
+            button.setForeground(null);
+        }
+        sinButton.setBackground(buttonBgColor);
+        cosButton.setBackground(buttonBgColor);
+        tanButton.setBackground(buttonBgColor);
+        decimalButton.setBackground(buttonBgColor);
+        signToggleButton.setBackground(buttonBgColor);
+        powerOfButton.setBackground(buttonBgColor);
+        backspaceButton.setBackground(buttonBgColor);
+        ceButton.setBackground(buttonBgColor);
+        eulerButton.setBackground(buttonBgColor);
+        PIButton.setBackground(buttonBgColor);
+        powerOfTenButton.setBackground(buttonBgColor);
+        sqrRootButton.setBackground(buttonBgColor);
+        logBaseTenButton.setBackground(buttonBgColor);
+        logBaseEButton.setBackground(buttonBgColor);
+        secondaryFunctionButton.setBackground(buttonBgColor);
+        xPowerOf2Button.setBackground(buttonBgColor);
+        secButton.setBackground(buttonBgColor);
+        cscButton.setBackground(buttonBgColor);
+        cotButton.setBackground(buttonBgColor);
+        moduloButton.setBackground(buttonBgColor);
+        calculateButton.setBackground(equalBgColor);
+        
+        //Change text color to default
+        sinButton.setForeground(null);
+        cosButton.setForeground(null);
+        tanButton.setForeground(null);
+        decimalButton.setForeground(null);
+        signToggleButton.setForeground(null);
+        powerOfButton.setForeground(null);
+        backspaceButton.setForeground(null);
+        ceButton.setForeground(null);
+        eulerButton.setForeground(null);
+        PIButton.setForeground(null);
+        powerOfTenButton.setForeground(null);
+        sqrRootButton.setForeground(null);
+        logBaseTenButton.setForeground(null);
+        logBaseEButton.setForeground(null);
+        secondaryFunctionButton.setForeground(null);
+        xPowerOf2Button.setForeground(null);
+        secButton.setForeground(null);
+        cscButton.setForeground(null);
+        cotButton.setForeground(null);
+        moduloButton.setForeground(null);
+        resultField.setForeground(null);
+        calculateButton.setForeground(null);
+    }
+    
+    //Getters and Setters
+    public JTextField getResultField() {
+		return resultField;
+	}
+
+	public JButton[] getNumButtons() {
+		return numButtons;
+	}
+
+	public JButton[] getOperationButtons() {
+		return operationButtons;
+	}
+
+	public JButton getCalculateButton() {
+		return calculateButton;
+	}
+
+	public JButton getSignToggleButton() {
+		return signToggleButton;
+	}
+
+	public JButton getSinButton() {
+		return sinButton;
+	}
+
+	public JButton getCosButton() {
+		return cosButton;
+	}
+
+	public JButton getTanButton() {
+		return tanButton;
+	}
+
+	public JButton getDecimalButton() {
+		return decimalButton;
+	}
+
+	public JButton getPowerOfButton() {
+		return powerOfButton;
+	}
+
+	public JButton getBackspaceButton() {
+		return backspaceButton;
+	}
+
+	public JButton getCeButton() {
+		return ceButton;
+	}
+
+	public JButton getEulerButton() {
+		return eulerButton;
+	}
+
+	public JButton getPIButton() {
+		return PIButton;
+	}
+
+	public JButton getPowerOfTenButton() {
+		return powerOfTenButton;
+	}
+
+	public JButton getSqrRootButton() {
+		return sqrRootButton;
+	}
+
+	public JButton getLogBaseTenButton() {
+		return logBaseTenButton;
+	}
+
+	public JButton getLogBaseEButton() {
+		return logBaseEButton;
+	}
+
+	public JButton getSecondaryFunctionButton() {
+		return secondaryFunctionButton;
+	}
+
+	public JButton getxPowerOf2Button() {
+		return xPowerOf2Button;
+	}
+
+	public JButton getSecButton() {
+		return secButton;
+	}
+
+	public JButton getCscButton() {
+		return cscButton;
+	}
+
+	public JButton getCotButton() {
+		return cotButton;
+	}
+
+	public JButton getModuloButton() {
+		return moduloButton;
+	}
 }
