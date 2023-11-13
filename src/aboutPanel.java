e7c8aab3f90fa65ad814230064df530dd25ae853
Brendan <33934145+BrendanoElTaco@users.noreply.github.com>
2023-11-13 10:33:53 -0500

    Updated about page

--- src/aboutPanel.java
+++ src/aboutPanel.java
@@ -19,11 +19,12 @@ public class aboutPanel extends JPanel {
 
         // Define the HTML with the hyperlink
         String htmlString = "<html><body><style>" 
-        					+ "<p style='font-family:\"Calibri\",sans-serif;'><strong><span style=\"font-size:12px;\">Version</span></strong><span style=\"font-size:12px;\">: 1.0.0</span></p>\r\n"
+        					+ "<p style='font-family:\"Calibri\",sans-serif;'><strong><span style=\"font-size:12px;\">Version</span></strong><span style=\"font-size:12px;\">: 1.0.1</span></p>\r\n"
         					+ "<p style='font-family:\"Calibri\",sans-serif;'><span style=\"font-size:12px;\">Developed by Brendan LeGrand</span></p>\r\n"
         					+ "<p style='font-family:\"Calibri\",sans-serif;'><span style=\"font-size:12px;\">For more information, visit my <a href=\"https://github.com/BrendanoElTaco/CSCI3300_Term_Project\">GitHub</a></span></p>\r\n"
         					+ "<p style='font-family:\"Calibri\",sans-serif;'><span style=\"font-size:12px;\">NumCruncher is a user-friendly calculator designed to perform basic arithmetic operations, trigonometric calculations, and more.</span></p>\r\n"
         					+ "<p style='font-family:\"Calibri\",sans-serif;'><span style=\"font-size:12px;\">It was developed as a part of a term project for my software development class.</span></p>\r\n"
+        					+ "<p style='font-family:\"Calibri\",sans-serif;'><span style=\"font-size:12px;\">If you find any bugs, please submit a bug report on <a href=\\\"https://github.com/BrendanoElTaco/CSCI3300_Term_Project\\\">GitHub</a>.</span></p>\r\n"
         					+ "<p style='font-family:\"Calibri\",sans-serif;'><span style=\"font-size:12px;\">&nbsp;</span></p>\r\n"
         					+ "<p style='font-family:\"Calibri\",sans-serif;'><strong><span style=\"font-size:12px;\">Credits</span></strong><span style=\"font-size:12px;\">:</span></p>\r\n"
         					+ "<p style='font-family:\"Calibri\",sans-serif;'><span style=\"font-size:12px;\">- Special thanks to Michael McLeod, MBT for teaching this class, and StackOverflow for saving my sanity.</span></p>\r\n"
